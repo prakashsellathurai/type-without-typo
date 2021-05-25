ab9fe2c16aca623a13f7a78b6c8483454509b3cf
prakash <prakashsellathurai@gmail.com>
2021-05-25 15:40:05 +0530

    basic flask setup done

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template,request
+from spell import correction
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def hello():
-    return "Hello World!"
-
+    return render_template('index.html')
+@app.route("/suggestions")
+def checker():
+    text = request.args.get('text')
+    return render_template('suggestion.html',suggestion=correction(text))
 if __name__ == "__main__":
     app.run()
